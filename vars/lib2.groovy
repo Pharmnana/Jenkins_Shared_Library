6ce5a46a4484b4dc0beb852f2ee3dd2dd96c462c
Nancy Sunday <142471122+Pharmnana@users.noreply.github.com>
2024-04-02 11:16:45 -0700

    Update lib2.groovy

--- vars/lib2.groovy
+++ vars/lib2.groovy
@@ -19,7 +19,7 @@ def call(String stageID){
 
     else if ("${stageID}" == "Staging"){
           echo 'Deploying to production tomcat_1'
-            deploy adapters: [tomcat9(credentialsId: 'tomcat_cred', path: '', url: ${STAGING_URL})], contextPath: null, war: 'target/*war'
+            deploy adapters: [tomcat9(credentialsId: 'tomcat_cred', path: '', url: ${env.STAGING_URL})], contextPath: null, war: 'target/*war'
           }
     
     else if ("${stageID}" == "Production"){
